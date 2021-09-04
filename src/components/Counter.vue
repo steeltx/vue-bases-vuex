91b39e421ad252e36adc20c7f9d5480a4a5a8c9d
steeltx <realsteel.gnu@gmail.com>
2021-09-03 23:56:48 -0500

    Lectura del state y mutations

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Counter - Vuex</h1>
+  <h2>Direct access: {{ $store.state.count }}</h2>
+  <h2>Computed : {{countComputed}}</h2>
+  
+  <button @click="increment">+1</button>
+  <button @click="incrementBy">+5</button>
+  <button>Random</button>
+  
+  <h1>mapState</h1>
+  <h2>mapState: {{ count }}</h2>
+  <h2>LastMutation: {{lastMutation}}</h2>
+</template>
+
+<script>
+import { mapState }from 'vuex'
+
+export default {
+
+    computed: {
+        countComputed(){
+            return this.$store.state.count
+        },
+        ...mapState(['count','lastMutation'])
+        // ...mapState({
+        //     count: state => state.count,
+        //     lastMutation : state => state.lastMutation
+        // })
+    },
+    methods: {
+        increment(){
+            this.$store.commit('increment')
+        },
+        incrementBy(){
+            this.$store.commit('incrementBy',5)
+        }
+    }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,18 @@ import { createStore } from 'vuex'
 export default createStore({
 
     state: {
-        count: 1
+        count: 1,
+        lastMutation: 'none'
+    },
+    mutations:{
+        increment( state ){
+            state.count++
+            state.lastMutation = 'increment'
+        },
+        incrementBy( state, val ){
+            state.count+=val
+            state.lastMutation = 'incrementBy'
+        }
     }
 
 })
